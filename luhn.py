309aea7891ebb02bb01198f081f979630578b2e8
Aaron Ash <aaron.ash@gmail.com>
2022-05-08 10:50:23 +1000

    luhny madness

---
+++ luhn.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# https://www.reddit.com/r/learnpython/comments/dkdnj6/luhn_algorithm/
+
+def main():
+    print("TODO: read all *.txt files")
+    print("TODO: numpy luhn algorithm")
+    print("TODO: count matches per file")
+
+if __name__=="__main__":
+    main()
